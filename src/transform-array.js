9fab276458407106cf452f1c8e3ecf5ef88b7ff2
Malev0lence <foliant230@gmail.com>
2020-10-29 18:45:21 +0300

    add: solve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,32 +1,31 @@
-// Your task is to implement the function transform(arr) that takes an array and returns transformed array, based on the control sequences that arr contains. Control sequences are defined string elements of the mentioned array:
-
-// --discard-next excludes the next element of the array from the transformed array.
-// --discard-prev excludes the previous element of the array from the transformed array.
-// --double-next doubles the next element of the array in the transformed array.
-// --double-prev doubles the previous element of the array in the transformed array.
-// For example:
-
-// transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
-
-// transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
-
-// The function must not affect inital array. Control sequences are applied from left to right. Control sequences do not fall into the transformed array. Control sequences in initial array don't occur in a row. If there is no element next to the control sequence to which it can be applied, it does nothing. The function should throw an Error if the arr is not an Array.
-
-// Write your code in src/transform-array.js.
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  let newArr = arr;
-  newArr.map((value, index) => {
-    if (value === '--discard-next') {
-      delete newArr[index + 1];
-    } else if (value === '--discard-prev') {
-      delete newArr[index - 1];
+  if (!arr || !Array.isArray(arr)) throw new Error;
+
+  let arrClone = [];
+  arr.forEach(element => {
+    arrClone.push(element);
+  });
+
+  let subArr = [];
+
+  arrClone.forEach((item, index) => {
+    if (item === '--double-next') {
+      if (arrClone[index + 1] === undefined) return;
+      subArr.push(arrClone[index + 1]);
+    } else if (item === '--double-prev') {
+        if (arrClone[index - 1] === undefined) return;
+        subArr.push(arrClone[index - 1]);
+    } else if (item === '--discard-prev') {
+        if (arrClone[index - 1] === undefined) return;
+        subArr.pop(arrClone[index - 1]);
+    } else if (item === '--discard-next') {
+        if (arrClone[index + 1] === undefined) return;
+        delete arrClone[index + 1];
+    } else {
+      subArr.push(arrClone[index])
     }
-    // } else if (value === '--double-next') {
-
-    // }
-    return newArr;
-  })
-
+  });
+  return subArr;
 }
