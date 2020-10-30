6c3177c7fd5161b7fc655c8a5e4c6004bee73ba2
Malev0lence <foliant230@gmail.com>
2020-10-30 21:43:13 +0300

    add: solve extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/*  */) {
-throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str += '';
+  if ('separator' in options === false) options.separator = '+';
+  if ('additionSeparator' in options === false) options.additionSeparator = '';
+  if ('addition' in options === false) options.addition = '';
+  if ('repeatTimes' in options === false || options.repeatTimes === undefined) options.repeatTimes = 1;
+  if ('additionRepeatTimes' in options === false || options.additionRepeatTimes === undefined) options.additionRepeatTimes = 0;
+
+  let subArr = [];
+  for (let i = 0; i < options.repeatTimes; i++) {
+    subArr.push(str);
+  }
+
+  if (options.additionRepeatTimes !== 0) {
+    subArr = subArr.map((item) => {
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      item = item + options.addition + options.additionSeparator;
+    }
+    return (options.additionSeparator.length !== 0) ? item.slice(0, -options.additionSeparator.length) : item;
+  })
+  } else {
+    subArr = subArr.map((item) => {
+    return item + options.addition;
+  })
+  }
+
+  subArr = subArr.join(options.separator);
+  return subArr
 };
   
