e527dcaa8a51981722bfd5c78cec211ef2cdf6c4
malev0lence <foliant230@gmail.com>
2020-11-02 08:22:54 +0300

    fix: fix naming

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,6 +1,6 @@
-var alf = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-class VigenereCipheringMachine {
+class VigenerecryptedStringMachine {
 
     constructor(isReverse) {
         if (isReverse === undefined) {
@@ -10,59 +10,59 @@ class VigenereCipheringMachine {
         }
     }
 
-    encrypt(text, key) {
-        if (!text || !key) throw new Error();
+    encrypt(str, key) {
+        if (!str || !key) throw new Error('Wrong str or no key');
 
-        var arrText = text.toLowerCase().split('');
+        var arrStr = str.toLowerCase().split('');
         var arrKey = key.toLowerCase().split('');
-        var arrDifference = [];
+        var arrDiff = [];
         var result = [];
-        for (var i = 0; i < arrText.length; i++) {
-            if (alf.includes(arrText[i])) {
-                if (arrKey.length < arrText.length) {
+        for (var i = 0; i < arrStr.length; i++) {
+            if (alphabet.includes(arrStr[i])) {
+                if (arrKey.length < arrStr.length) {
                     arrKey.push(arrKey[i]);
                 }
-                if (((alf.indexOf(arrText[i])) + (alf.indexOf(arrKey[i])) >= 26)) {
-                    arrDifference.push(((alf.indexOf(arrText[i])) + (alf.indexOf(arrKey[i]))) - 26);
+                if (((alphabet.indexOf(arrStr[i])) + (alphabet.indexOf(arrKey[i])) >= 26)) {
+                    arrDiff.push(((alphabet.indexOf(arrStr[i])) + (alphabet.indexOf(arrKey[i]))) - 26);
                 } else {
-                    arrDifference.push(((alf.indexOf(arrText[i])) + (alf.indexOf(arrKey[i]))));
+                    arrDiff.push(((alphabet.indexOf(arrStr[i])) + (alphabet.indexOf(arrKey[i]))));
                 }
-                result.push(alf[arrDifference[i]]);
+                result.push(alphabet[arrDiff[i]]);
             } else {
-                result.push(arrText[i]);
+                result.push(arrStr[i]);
                 arrKey.splice(i, 0, ' ');
-                arrDifference.splice(i, 0, arrText[i]);
+                arrDiff.splice(i, 0, arrStr[i]);
             }
         }
         return this.isReverse ? result.join('').toUpperCase() : result.reverse().join('').toUpperCase();
     }
 
-    decrypt(cipher, key) {
-        if (!cipher || !key) throw new Error();
+    decrypt(cryptedStr, key) {
+        if (!cryptedStr || !key) throw new Error();
 
-        var arrCipher = cipher.toLowerCase().split('');
+        var arrCryptedStr = cryptedStr.toLowerCase().split('');
         var arrKey = key.toLowerCase().split('');
-        var arrDifference = [];
+        var arrDiff = [];
         var result = [];
-        for (var i = 0; i < arrCipher.length; i++) {
-            if (alf.includes(arrCipher[i])) {
-                if (arrKey.length < arrCipher.length) {
+        for (var i = 0; i < arrCryptedStr.length; i++) {
+            if (alphabet.includes(arrCryptedStr[i])) {
+                if (arrKey.length < arrCryptedStr.length) {
                     arrKey.push(arrKey[i]);
                 }
-                if (alf.indexOf(arrCipher[i]) < alf.indexOf(arrKey[i])) {
-                    arrDifference.push(((alf.indexOf(arrCipher[i])) - (alf.indexOf(arrKey[i]))) + 27);
+                if (alphabet.indexOf(arrCryptedStr[i]) < alphabet.indexOf(arrKey[i])) {
+                    arrDiff.push(((alphabet.indexOf(arrCryptedStr[i])) - (alphabet.indexOf(arrKey[i]))) + 27);
                 } else {
-                    arrDifference.push(((alf.indexOf(arrCipher[i])) - (alf.indexOf(arrKey[i]))) + 1);
+                    arrDiff.push(((alphabet.indexOf(arrCryptedStr[i])) - (alphabet.indexOf(arrKey[i]))) + 1);
                 }
-                result.push(alf[arrDifference[i] - 1]);
+                result.push(alphabet[arrDiff[i] - 1]);
             } else {
-                result.push(arrCipher[i]);
+                result.push(arrCryptedStr[i]);
                 arrKey.splice(i, 0, ' ');
-                arrDifference.splice(i, 0, arrCipher[i]);
+                arrDiff.splice(i, 0, arrCryptedStr[i]);
             }
         }
         return this.isReverse ? result.join('').toUpperCase() : result.reverse().join('').toUpperCase();
     }
 }
 
-module.exports = VigenereCipheringMachine;
+module.exports = VigenerecryptedStringMachine;
